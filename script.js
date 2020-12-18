5ce53882b7f8c5204d85aa514c0c3d05fba21af2
soutatori <10ruoa05@gmail.com>
2020-12-18 21:11:44 +0900

    testtest

---
+++ script.js
@@ -0,0 +1,66 @@
+for(var star = 1; star <= 5; star++)
+{document.write("★");}
+
+document.write("<br><br>");
+
+for(var star1=1;star1<=2;star1++)
+   {for(star2=1;star2<=3;star2++)
+   {document.write("★");}
+    document.write("<br>");}
+
+document.write("<br><br>");
+
+for(var star1=1;star1<=2;star1++)
+   {for(star2=1;star2<=5;star2++)
+   {document.write("☆");}
+    document.write("<br>");}
+
+document.write("<br><br>");
+
+for(var star1=1;star1<=4;star1++)
+   {for(star2=1;star2<=5;star2++)
+   {document.write("★");}
+    document.write("<br>");}
+
+document.write("<br><br>");
+
+for(var star1=1;star1<=4;star1++)
+   {for(star2=1;star2<=3;star2++)
+   {document.write("★");}
+    document.write("<br>");}
+
+document.write("<br><br>");
+
+for(var no1=0;no1<3;no1++)
+    {for(var no2=0;no2<3;no2++)
+    {if(no2%2==0)
+    {document.write("★");}
+     else{document.write("☆");}}
+     document.write("<br>");}
+
+document.write("<br><br>");
+
+for(var no1=0;no1<4;no1++)
+    {for(var no2=0;no2<5;no2++)
+    {if(no2%2==0)
+    {document.write("★");}
+     else{document.write("☆");}}
+     document.write("<br>");}
+
+document.write("<br><br>");
+
+for(var x=0;x<5;x=x+1)
+    {for(var y=0;y<x+1;y=y+1)
+    {document.write("★");}
+     document.write("<br>");}
+
+
+
+
+
+
+
+
+
+
+
